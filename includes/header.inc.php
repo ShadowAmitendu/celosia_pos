2df16fe107ed7df8d4913fe52eea23b5eedd118d
Amitendu Bikash Dhusiya <amitendubikashdhusiya@gmail.com>
2025-09-30 15:18:39 +0530

    added Favicon for the site!

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -4,10 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0">
-    <link rel="apple-touch-icon" sizes="180x180" href="../assets/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../assets/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="../assets/favicon/favicon-16x16.png">
-    <link rel="manifest" href="../assets/favicon/site.webmanifest">
+    <link rel="apple-touch-icon"
+          sizes="180x180"
+          href="../assets/favicon/apple-touch-icon.png">
+    <link rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="../assets/favicon/favicon-32x32.png">
+    <link rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="../assets/favicon/favicon-16x16.png">
+    <link rel="manifest"
+          href="../assets/favicon/site.webmanifest">
     <title><?php echo $pageTitle ?? 'CELOSIA CANDLES POS'; ?></title>
 
     <!-- Tailwind CSS -->
@@ -83,7 +92,7 @@
                     </svg>
                 </div>
                 <div class="text-sm md:text-base">
-                    <h1 class="heading-font text-lg md:text-2xl font-bold text-gray-800">@CELOSIACANDLES</h1>
+                    <h1 class="heading-font text-lg md:text-2xl font-bold text-gray-800">CELOSIA CANDLES - POS</h1>
                     <p class="text-xs text-gray-600">Handcrafted with Love</p>
                 </div>
             </a>
